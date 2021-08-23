0346852b9fd6d596794c5626d7c7da7008c5cf30
Nickolaj Andersen <nickolaj@outlook.com>
2021-08-23 16:13:25 +0200

    Removed

--- Deploy/test.ps1
+++
@@ -1,8 +0,0 @@
-$Key = ""
-$DecodedKey = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($Key))
-$PSObject = [PSCustomObject]@{
-    "Prefix" = $DecodedKey.SubString(0,21)
-    "Thumbprint" = $DecodedKey.Split(">")[1].SubString(0,40)
-    "PublicKeyHash" = $DecodedKey.Split(">")[1].SubString(40)
-}
-$PSObject
